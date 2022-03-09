57116fa5ec0715e77dd146af98263fbd58aef874
Ashish Kumar <99878223+Surajthaku1@users.noreply.github.com>
2022-03-09 18:21:34 +0530

    Add files via upload

---
+++ ashish sql.sql
@@ -0,0 +1,16 @@
+create database yu
+use yu
+celect * from rtreate table rt(id int identity,name varchar(50), salary int , roll varchar(79))
+
+s
+insert into rt(name,salary,roll)values('dfs',89,90)
+update rt set name = 'amit' where id =4 
+sp_rename 'rt.salary',ssalary
+select * from rt
+alter table rt add address varchar(80);
+update rt set address = 'modinagar' where id = 1
+update rt set address = 'modi' where id = 2
+update rt set address = 'nagar' where id = 3
+update rt set address = 'dinagar' where id = 4
+update rt set address = 'odinagar' where id = 5
+   
